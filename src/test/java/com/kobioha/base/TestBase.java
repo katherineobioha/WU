678513037456e19200da82d46745adc3d1829d4a
Katherine obioha <katobioha@gmail.com>
2019-09-27 23:14:56 -0700

    committing reusability functionalities in testbase class

--- src/test/java/com/kobioha/base/TestBase.java
+++ src/test/java/com/kobioha/base/TestBase.java
@@ -1,5 +1,90 @@
 package com.kobioha.base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class TestBase {
 
+	private static WebDriver driver;
+	
+	WebDriverWait wait;
+	
+	public WebDriver getWebDriver()
+	{
+		return this.driver;
+	}
+	
+	public boolean createDriver()
+	{
+		if(driver==null)
+		{
+			System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver");
+			ChromeOptions options = new ChromeOptions();
+			options.setExperimentalOption("useAutomationExtension", false);
+			this.driver = new ChromeDriver(options);
+		}
+		else
+		{
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean SendKeysToElement(String keys, WebElement elem)
+	{
+		elem.sendKeys(keys);
+		return true;
+	}
+	
+	public boolean ClickOnElements(WebElement elem)
+	{
+		wait = new WebDriverWait(driver, 4);
+		if(elem.isDisplayed() && elem.isEnabled())
+		{
+			elem.click();
+			return true;
+		}
+		else
+		{
+			System.out.println("Could not find elem" + elem);
+			return false;
+		}
+		
+	}
+	
+	public void ScrollToTop()
+	{
+		((JavascriptExecutor) driver).executeScript("window.scrollTo(0,0)");
+	}
+	
+	public String scrollToElement(String pos, WebElement elem)
+	{
+		if(pos.equalsIgnoreCase("down"))
+		{
+			Actions act = new Actions(driver);
+			act.moveToElement(elem).click();
+			act.sendKeys(Keys.PAGE_DOWN).perform();
+			act.moveToElement(elem).click();
+		}
+		else
+		{
+			Actions act = new Actions(driver);
+			act.moveToElement(elem).click();
+			act.sendKeys(Keys.PAGE_UP).perform();
+			act.moveToElement(elem).click();
+		}
+		
+		return elem.getText();
+	}
+	
+	public String parseAndSearchString(String s, WebElement onetimefee)
+	{
+		return null;
+	}
 }
--- src/test/java/com/kobioha/pom/Home.java
+++ src/test/java/com/kobioha/pom/Home.java
@@ -1,9 +1,77 @@
 package com.kobioha.pom;
 
-public class Home {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.kobioha.base.TestBase;
+
+public class Home extends TestBase{
 
 	@FindBy(tagName = "h1")
-	WebElement headertitile;
+	WebElement headertitle;
+	
+	@FindBy(xpath="//*[@id='option_3']/li[1]")
+	WebElement phoneNumber;
+	
+	@FindBy(xpath="//*[@id='option_3']/li[2]")
+	WebElement email;
+	
+	@FindBy(xpath="//*[@id='option_3']/li[1]")
+	WebElement address;
+	
+	@FindBy(xpath="")
+	WebElement membership;
+
+	@FindBy(xpath="//*[@id='option_3']/li[1]")
+	WebElement coreMembership;
+	
+	@FindBy(xpath="//*[@id='option_3']/li[1]")
+	WebElement contact;
+	
+	public Home()
+	{
+		PageFactory.initElements(getWebDriver(), this);
+	}
+	
+	public void navigateToWebsite()
+	{
+		getWebDriver().get("https://wheelsup.com");
+	}
+	
+	public WebElement getTitle()
+	{
+		return headertitle;
+	}
+	
+	public WebElement getHeadertitle() {
+		return headertitle;
+	}
+
+	public WebElement getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public WebElement getEmail() {
+		return email;
+	}
+
+	public WebElement getAddress() {
+		return address;
+	}
+
+	public WebElement getMembership() {
+		return membership;
+	}
+
+	public WebElement getCoreMembership() {
+		return coreMembership;
+	}
+
+	public WebElement getContact() {
+		return contact;
+	}
+	
 	
 	
 }
